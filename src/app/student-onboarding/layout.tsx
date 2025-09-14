f828d0c2ad97a62cdaa719eef5dcc5c1a3d74082
Anupam Jain <anupam99jain@gmail.com>
2025-09-14 03:50:49 +0000

    Try fixing this error: `Runtime Error: Error: useAppContext must be used

--- src/app/student-onboarding/layout.tsx
+++ src/app/student-onboarding/layout.tsx
@@ -1,4 +1,6 @@
+
 import { FlourishULogo } from "@/components/logo";
+import { AppContextProvider } from "@/app/(app)/app-context";
 
 export default function OnboardingLayout({
   children,
@@ -6,13 +8,15 @@ export default function OnboardingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4">
-      <div className="absolute top-8 left-8">
-        <FlourishULogo />
+    <AppContextProvider>
+      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4">
+        <div className="absolute top-8 left-8">
+          <FlourishULogo />
+        </div>
+        <main className="w-full max-w-xl">
+          {children}
+        </main>
       </div>
-      <main className="w-full max-w-xl">
-        {children}
-      </main>
-    </div>
+    </AppContextProvider>
   );
 }
